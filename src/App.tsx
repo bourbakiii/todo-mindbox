951022f3ae3a791ea9bf8014bd100652fa458260
bourbakiii <gabisov.basis@gmail.com>
2024-08-18 17:52:56 +0300

    Installed ANTD

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "antd";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,16 +18,19 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Vercel deployes succesfully
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p>
+                Vercel deployed succesfully
+            </p>
+            <div className="App">
+                <Button type="primary">Antd Button</Button>
+            </div>
+        </div>
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
       </p>
     </>
   )
